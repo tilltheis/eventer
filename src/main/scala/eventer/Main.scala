af66b28a57aed9840fcfe633f716564b163944e3
Till Theis <theis.till@gmail.com>
2020-12-29 00:56:49 +0100

    modularized Main using ZLayers

--- src/main/scala/eventer/Main.scala
+++ src/main/scala/eventer/Main.scala
@@ -1,18 +1,21 @@
 package eventer
 
-import java.util.UUID
 import com.typesafe.scalalogging.StrictLogging
-import eventer.application.{EventRoutes, JwtsImpl, Middlewares, SessionRoutes, UserRoutes, WebServer}
+import eventer.application._
 import eventer.domain._
 import eventer.domain.session.SessionServiceImpl
+import eventer.domain.user.UserRepository
 import eventer.infrastructure.BlowfishCryptoHashing.BlowfishHash
 import eventer.infrastructure.EmailSenderImpl.PasswordAuthentication
 import eventer.infrastructure._
+import org.http4s.server.HttpMiddleware
 import pureconfig.ConfigSource
 import pureconfig.generic.auto._
 import zio.blocking.Blocking
 import zio.clock.Clock
-import zio.{UIO, URIO, URLayer, ZIO, ZLayer}
+import zio._
+
+import java.util.UUID
 
 final case class ServerConfig(port: Int,
                               jwtSigningKeyBase64: String,
@@ -23,65 +26,78 @@ final case class EmailConfig(sender: String, host: String, port: Int, username: 
 final case class Config(publicUrl: String, server: ServerConfig, db: DbConfig, email: EmailConfig)
 
 object Main extends zio.App with StrictLogging {
-  type MainEnvironment = Clock with Blocking
-  type ApplicationEnvironment = Clock with Blocking with DatabaseContext
-
-  def applicationLayer(config: Config): URLayer[MainEnvironment, ApplicationEnvironment] = {
-    val ctx = DatabaseProvider.withMigration(config.db.quillConfigKey).map(_.get.database)
-    ctx ++ ZLayer.requires[Blocking] ++ ZLayer.requires[Clock]
-  }
-
-  def application(config: Config): ZIO[ApplicationEnvironment, Throwable, Unit] = {
-    ZIO.service[DatabaseContext.Service].flatMap { dbCtx =>
-      ZIO.service[Clock.Service].flatMap { clock =>
-        val userRepository = new DbUserRepository[BlowfishHash](dbCtx, _.hash, BlowfishHash.unsafeFromHashString)
-        val eventRepository = new DbEventRepository(dbCtx)
-        val emailSender =
-          new EmailSenderImpl(config.email.host,
-                              config.email.port,
-                              PasswordAuthentication(config.email.username, config.email.password))
-        val cryptoHashing = new BlowfishCryptoHashing()
-
-        (for {
-          // example user with email "example@example.org" and password "password"
-          _ <- userRepository
-            .create(User(
-              UserId(UUID.fromString("6f31ccde-4321-4cc9-9056-6c3cbd550cba")),
-              "Till",
-              "example@example.org",
-              BlowfishHash.unsafeFromHashString("$2a$10$d.vQEHwPIqtSYWQOMtg7LuZgTOx1R/2sOLnqCUkpixkXJ1paUhEIm")
-            ))
-            .option
-            .absorb
-            .ignore // catch duplicate insert exceptions
-          jwtKey <- util.secretKeyFromBase64(config.server.jwtSigningKeyBase64, JwtsImpl.JwtSigningAlgorithm)
-          csrfKey <- util.secretKeyFromBase64(config.server.csrfSigningKeyBase64, Middlewares.CsrfSigningAlgorithm)
-          sessionService = new SessionServiceImpl(userRepository, cryptoHashing)
-          jwts = new JwtsImpl(jwtKey, clock)
-          webServer = new WebServer(
-            eventRoutes =
-              new EventRoutes(eventRepository, UIO(EventId(UUID.randomUUID())), Middlewares.auth(jwts)).routes,
-            sessionRoutes = new SessionRoutes(clock,
-                                              jwts,
-                                              sessionService,
-                                              Middlewares.auth(jwts),
-                                              config.server.useSecureCookies).routes,
-            userRoutes =
-              new UserRoutes(userRepository, emailSender, cryptoHashing, UIO(UserId(UUID.randomUUID()))).routes,
-            Middlewares.csrf(csrfKey, config.server.useSecureCookies)
-          )
-          serving <- webServer.serve(config.server.port)
-        } yield serving).unit
-      }
+  val insertDemoUserIntoDb: URIO[Has[UserRepository[BlowfishHash]], Unit] =
+    ZIO.service[UserRepository[BlowfishHash]].flatMap {
+      _.create(
+        User(
+          UserId(UUID.fromString("6f31ccde-4321-4cc9-9056-6c3cbd550cba")),
+          "Till",
+          "example@example.org",
+          BlowfishHash.unsafeFromHashString("$2a$10$d.vQEHwPIqtSYWQOMtg7LuZgTOx1R/2sOLnqCUkpixkXJ1paUhEIm")
+        )).option.absorb.ignore // catch duplicate insert exceptions
     }
+
+  val configLayer: TaskLayer[Has[Config]] = UIO(ConfigSource.default.at("eventer").loadOrThrow[Config]).absorb.toLayer
+
+  val jwtsLayer: RLayer[Clock with Has[Config], Has[Jwts]] = (for {
+    (clock, config) <- ZIO.services[Clock.Service, Config]
+    jwtKey <- util.secretKeyFromBase64(config.server.jwtSigningKeyBase64, JwtsImpl.JwtSigningAlgorithm)
+  } yield new JwtsImpl(jwtKey, clock)).toLayer
+
+  val databaseContextLayer: RLayer[Blocking with Has[Config], DatabaseContext] =
+    ZLayer.fromServiceManaged[Config, Blocking, Nothing, DatabaseContext.Service]((c: Config) =>
+      DatabaseProvider.withMigration(c.db.quillConfigKey).map(_.get.database.get).build)
+
+  val eventRoutesLayer: RLayer[DatabaseContext with Has[Jwts], Has[EventRoutes]] =
+    (for {
+      (dbCtx, jwts) <- ZIO.services[DatabaseContext.Service, Jwts]
+      eventRepository = new DbEventRepository(dbCtx)
+    } yield new EventRoutes(eventRepository, UIO(EventId(UUID.randomUUID())), Middlewares.auth(jwts))).toLayer
+
+  val sessionRoutesLayer: RLayer[DatabaseContext with Clock with Has[Jwts] with Has[Config], Has[SessionRoutes]] =
+    (for {
+      (clock, config, dbCtx, jwts) <- ZIO.services[Clock.Service, Config, DatabaseContext.Service, Jwts]
+      authMiddleware = Middlewares.auth(jwts)
+      userRepository = new DbUserRepository[BlowfishHash](dbCtx, _.hash, BlowfishHash.unsafeFromHashString)
+      sessionService = new SessionServiceImpl(userRepository, new BlowfishCryptoHashing)
+    } yield new SessionRoutes(clock, jwts, sessionService, authMiddleware, config.server.useSecureCookies)).toLayer
+
+  val userRoutesLayer: RLayer[Has[Config] with DatabaseContext, Has[UserRoutes[BlowfishHash]]] = (for {
+    (config, dbCtx) <- ZIO.services[Config, DatabaseContext.Service]
+    userRepository = new DbUserRepository[BlowfishHash](dbCtx, _.hash, BlowfishHash.unsafeFromHashString)
+    _ <- insertDemoUserIntoDb.provideLayer(ZLayer.succeed(userRepository: UserRepository[BlowfishHash]))
+    emailSender = new EmailSenderImpl(config.email.host,
+                                      config.email.port,
+                                      PasswordAuthentication(config.email.username, config.email.password))
+  } yield
+    new UserRoutes(userRepository, emailSender, new BlowfishCryptoHashing, UIO(UserId(UUID.randomUUID())))).toLayer
+
+  val csrfMiddlewareLayer: RLayer[Has[Config], Has[HttpMiddleware[Task]]] = (for {
+    config <- ZIO.service[Config]
+    key <- util.secretKeyFromBase64(config.server.csrfSigningKeyBase64, Middlewares.CsrfSigningAlgorithm)
+  } yield Middlewares.csrf(key, config.server.useSecureCookies)).toLayer
+
+  val webServerLayer: URLayer[
+    Has[EventRoutes] with Has[SessionRoutes] with Has[UserRoutes[BlowfishHash]] with Has[HttpMiddleware[Task]],
+    Has[WebServer]] =
+    ZLayer.fromServices[EventRoutes, SessionRoutes, UserRoutes[BlowfishHash], HttpMiddleware[Task], WebServer](
+      (eventRoutes, sessionRoutes, userRoutes, csrfMiddleware) =>
+        new WebServer(eventRoutes.routes, sessionRoutes.routes, userRoutes.routes, csrfMiddleware))
+
+  val appLayer: RLayer[ZEnv, Has[Config] with Has[WebServer]] = {
+    val routesLayers = eventRoutesLayer ++ sessionRoutesLayer ++ userRoutesLayer ++ csrfMiddlewareLayer
+    ZLayer.requires[ZEnv] >+> configLayer >+> (jwtsLayer ++ databaseContextLayer) >+> routesLayers >+> webServerLayer
   }
+
+  val runApp: RIO[Has[Config] with Has[WebServer], Unit] =
+    for {
+      (config, webServer) <- ZIO.services[Config, WebServer]
+      result <- webServer.serve(config.server.port)
+    } yield result
 
   override def run(args: List[String]): URIO[zio.ZEnv, zio.ExitCode] =
-    for {
-      config <- UIO(ConfigSource.default.at("eventer").loadOrThrow[Config])
-      result <- application(config)
-        .provideLayer(applicationLayer(config))
-        .mapError(logger.error("Something went wrong!", _))
-        .exitCode
-    } yield result
+    runApp
+      .provideCustomLayer(appLayer)
+      .mapError(logger.error("Something went wrong!", _))
+      .exitCode
 }
